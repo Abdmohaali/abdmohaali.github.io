4af59c8270d2ec1a39127b5e8c1e20ef52f8200e
Abdalla Mohamed Ali <abdmohaali@gmail.com>
2026-02-22 20:14:59 -0800

    cooked

--- HW2/js/script.js
+++ HW2/js/script.js
@@ -6,15 +6,31 @@ document.querySelector("#symbolContainer3")
 ]
 let symbols = ["🍒", "❤️", "🎶"]
 
-gambleBtn.addEventListener("click", function shuffle(reels){
-    let currentIndex = reels.length;
+gambleBtn.addEventListener("click", function shuffle(symbols){
+    let currentIndex = symbols.length;
     while(currentIndex != 0){
         let randomIndex = Math.floor(Math.random() * currentIndex);
         currentIndex--;
         [reels[currentIndex], reels[randomIndex]] = 
         [reels[randomIndex], reels[currentIndex]];
     }
+
+    if(reels[0] == reels[1] && reels[1] == reels[2]){
+        totalMoney *= 3;
+    } else {
+        totalMoney -= 100;
+    }
+
+    if(totalMoney == 0){
+        console.log("You Lose, womp womp");
+    }
 });
+
+// totalMoney.addEventListener("input", function(){
+
+// });
+
+
 
 
 
