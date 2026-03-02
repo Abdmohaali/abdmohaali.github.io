f81ae8803c28fb13ac2797666fc19e778d0b5428
Abdalla Mohamed Ali <abdmohaali@gmail.com>
2026-03-01 21:46:13 -0800

    initialcommit

--- lab4/js/script.js
+++ lab4/js/script.js
@@ -1,45 +1,69 @@
-
-async function pageLoad() {
-    //any code that happens when the page loads goes here
-}
-pageLoad();
-let zipCodeInput = document.querySelector("#zipCodeInput");
-zipCodeInput.addEventListener("input", async function () {
-    let url = `https://csumb.space/api/cityInfoAPI.php?zip=${zipCodeInput.value}`;
-    try {
-        const response = await fetch(url);
-        if (!response.ok) {
-            throw new Error("Error accessing API endpoint");
-        }
-        const data = await response.json();
-        console.log(data);
-        document.querySelector("#cityDisplay").textContent = data.city;
-        document.querySelector("#latitudeDisplay").textContent = data.latitude;
-        document.querySelector("#longitudeDisplay").textContent = data.longitude;
-
-    } catch (err) {
-        if (err instanceof TypeError) {
-            alert("Error accessing API endpoint (network failure)");
-        } else {
-            alert(err.message);
-        }
-    } //catch
+document.querySelector("#zip").addEventListener("change", displayCity);
+document.querySelector("#state").addEventListener("change", displayCounties);
+document.querySelector("#username").addEventListener("change", checkUsername);
+document.querySelector("#signupForm").addEventListener("submit", function(event) {
+    validateForm(event);
 });
+document.querySelector("#password").addEventListener("change", checkPassword);
 
-let usernameInput = document.querySelector("#usernameInput");
-usernameInput.addEventListener("input", async function () {
-    let url = https://csumb.space/api/usernamesAPI.php?username=${usernameInput.value} ;
-    try {
-        const response = await fetch(url);
-        if (!response.ok) {
-            throw new Error("Error accessing API endpoint");
-        }
-        const data = await response.json();
-        document.querySelector("#usernameDisplay").textContent = data.usernameInput;
-    }catch (err) {
-        if (err instanceof TypeError) {
-            alert("Error accessing API endpoint (network failure)");
-        } else {
-            alert(err.message);
-        }
-    });
+async function displayCity() {
+    let zipCode = document.querySelector("#zip").value;
+    
+    let url =`https://csumb.space/api/cityInfoAPI.php?zip=${zipCode}`;
+    let response = await fetch(url);
+    let data = await response.json();
+    // console.log(data);
+    document.querySelector("#city").innerHTML = data.city;
+    document.querySelector("#latitude").innerHTML = data.latitude;
+    document.querySelector("#longitude").innerHTML = data.longitude;
+}
+
+async function displayCounties() {
+    let state = document.querySelector("#state").value;
+    let url = `https://csumb.space/api/countyListAPI.php?state=${state}`;
+    let response = await fetch(url);
+    let data = await response.json();
+    let countyList = document.querySelector("#county");
+    countyList.innerHTML = "<option> Select County </option>"
+    for(let i=0; i < data.length; i++){
+        countyList.innerHTML += `<option> ${data[i].county} </option>`;
+    }
+}
+
+async function checkUsername() {
+    let username = document.querySelector("#username").value;
+    let url = `https://csumb.space/api/usernamesAPI.php?username=${username}`;
+    let response = await fetch(url);
+    let data = await response.json();
+    let usernameError = document.querySelector("#usernameError")
+    if(data.available){
+        usernameError.innerHTML = "Username available!";
+        username.style.color = "green";
+    }
+    else {
+        usernameError.innerHTML = "Username taken";
+        username.style.color = "red";
+    }
+}
+
+async function checkPassword() {
+    let password = document.querySelector("#password").value;
+    let url = `https://csumb.space/api/suggestedPassword.php?length=6`;
+    let response = await fetch(url);
+    let data = await response.json();
+    let suggestedPassword = document.querySelector("#suggestedPwd")
+}
+
+function validateForm(e){
+    let isValid = true;
+    let username = document.querySelector("#username").value;
+    if (username.length == 0){
+        document.querySelector("#usernameError").innerHTML = "Username Required!";
+        isValid = false;
+    }
+    if (password.length)
+
+    if(!isValid) {
+        e.preventDefault();
+    }
+}
